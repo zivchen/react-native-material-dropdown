e5d1cba81e12bfe859aa125453581bb711490073
zivchen <zivchenking@gmail.com>
2018-06-17 11:42:52 +0300

    formating

--- src/components/dropdown/styles.js
+++ src/components/dropdown/styles.js
@@ -1,66 +1,69 @@
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Platform } from "react-native";
 
 export default StyleSheet.create({
   accessory: {
     width: 24,
     height: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
 
   triangle: {
     width: 8,
     height: 8,
-    transform: [{
-      translateY: -4,
-    }, {
-      rotate: '45deg',
-    }],
+    transform: [
+      {
+        translateY: -4
+      },
+      {
+        rotate: "45deg"
+      }
+    ]
   },
 
   triangleContainer: {
     width: 12,
     height: 6,
-    overflow: 'hidden',
-    alignItems: 'center',
+    overflow: "hidden",
+    alignItems: "center",
 
-    backgroundColor: 'transparent', /* XXX: Required */
+    backgroundColor: "transparent" /* XXX: Required */
   },
 
   overlay: {
-  //  ...StyleSheet.absoluteFillObject,
+    //  ...StyleSheet.absoluteFillObject,
   },
 
   picker: {
-    backgroundColor: 'rgba(255, 255, 255, 1.0)',
+    backgroundColor: "rgba(255, 255, 255, 1.0)",
     borderRadius: 2,
 
-    position: 'absolute',
+    position: "absolute",
 
     ...Platform.select({
       ios: {
         shadowRadius: 2,
-        shadowColor: 'rgba(0, 0, 0, 1.0)',
+        shadowColor: "rgba(0, 0, 0, 1.0)",
         shadowOpacity: 0.54,
-        shadowOffset: { width: 0, height: 2 },
+        shadowOffset: { width: 0, height: 2 }
       },
 
       android: {
-        elevation: 2,
-      },
-    }),
+        elevation: 2
+      }
+    })
   },
 
   item: {
-    textAlign: 'left',
+    textAlign: "left"
   },
 
   scroll: {
     flex: 1,
-    borderRadius: 2,
+    borderRadius: 2
   },
 
   scrollContainer: {
-    paddingVertical: 8,
-  },
+    paddingVertical: 8
+  }
 });
